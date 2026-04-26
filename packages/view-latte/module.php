0ee1ecb0d871529fe70dd9e47f73c8350328707b
Evan McDan <ivanpaldan@yandex.com>
2026-04-26 19:22:33 +0000

    fix: register PermissionRegistry binding and install Latte view driver
    - Register Marko\AdminAuth\Contracts\PermissionRegistryInterface in packages/admin-auth/module.php
    - Install marko/view-latte and move it to packages/ for proper discovery
    - Update composer.json to handle local marko/* packages via replace
    - Prepend MarkoAutoloader in bootstrap/autoload.php to ensure local packages take precedence

---
+++ packages/view-latte/module.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use Marko\Core\Container\ContainerInterface;
+use Marko\View\Latte\LatteEngineFactory;
+use Marko\View\Latte\LatteView;
+use Marko\View\TemplateResolverInterface;
+use Marko\View\ViewInterface;
+
+return [
+    'bindings' => [
+        ViewInterface::class => function (ContainerInterface $container): ViewInterface {
+            $engine = $container->get(LatteEngineFactory::class)->create();
+            $resolver = $container->get(TemplateResolverInterface::class);
+
+            return new LatteView($engine, $resolver);
+        },
+    ],
+];
